7515d76ccccf635b3830e7203d3976adb4c0c782
👻 <y@uklok.ai>
2024-05-24 01:07:37 -0400

    feat(hardhat): 👻 Networks | Chainlink Config
    - The potentially networks to be used for deployment were updated.
    - All the details required for the scripts were included.

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -67,10 +67,30 @@ const config: ChainLinkUserConfig = {
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+      },
+      functions: {
+        router: "0x65Dcc24F8ff9e51F10DCc7Ed1e4e2A61e6E14bd6",
+        donId: {
+          onChain: "0x66756e2d657468657265756d2d6d61696e6e65742d3100000000000000000000",
+          offChain: "fun-ethereum-mainnet-1",
+        },
+      },
     },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+      },
+      functions: {
+        router: "0xb83E47C2bC239B3bf370bc41e1459A34b41238D0",
+        donId: {
+          onChain: "0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000",
+          offChain: "fun-ethereum-sepolia-1",
+        },
+      },
     },
     arbitrum: {
       url: `https://arb-mainnet.g.alchemy.com/v2/${providerApiKey}`,
@@ -87,14 +107,58 @@ const config: ChainLinkUserConfig = {
     optimismSepolia: {
       url: `https://opt-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0xE4aB69C077896252FAFBD49EFD26B5D171A32410",
+      },
+      functions: {
+        router: "0xC17094E3A1348E5C7544D4fF8A36c28f2C6AAE28",
+        donId: {
+          onChain: "0x66756e2d6f7074696d69736d2d7365706f6c69612d3100000000000000000000",
+          offChain: "fun-optimism-sepolia-1",
+        },
+      },
     },
     polygon: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0xb0897686c545045aFc77CF20eC7A532E3120E0F1",
+      },
+      functions: {
+        router: "0xdc2AAF042Aeff2E68B3e8E33F19e4B9fA7C73F10",
+        donId: {
+          onChain: "0x66756e2d706f6c79676f6e2d6d61696e6e65742d310000000000000000000000",
+          offChain: "fun-polygon-mainnet-1",
+        },
+      },
     },
     polygonMumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+      },
+      functions: {
+        router: "0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C",
+        donId: {
+          onChain: "0x66756e2d706f6c79676f6e2d6d756d6261692d31000000000000000000000000",
+          offChain: "fun-polygon-mumbai-1",
+        },
+      },
+    },
+    polygonAmoy: {
+      url: `https://polygon-amoy.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904",
+      },
+      functions: {
+        router: "0xC22a79eBA640940ABB6dF0f7982cc119578E11De",
+        donId: {
+          onChain: "0x66756e2d706f6c79676f6e2d616d6f792d310000000000000000000000000000",
+          offChain: "fun-polygon-amoy-1",
+        },
+      },
     },
     polygonZkEvm: {
       url: `https://polygonzkevm-mainnet.g.alchemy.com/v2/${providerApiKey}`,
@@ -155,6 +219,38 @@ const config: ChainLinkUserConfig = {
     modeSepolia: {
       url: "https://sepolia.mode.network",
       accounts: [deployerPrivateKey],
+    },
+    axax: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0x5947BB275c521040051D82396192181b413227A3",
+      },
+      functions: {
+        router: "0x9f82a6A0758517FD0AfA463820F586999AF314a0",
+        donId: {
+          onChain: "0x66756e2d6176616c616e6368652d6d61696e6e65742d31000000000000000000",
+          offChain: "fun-avalanche-mainnet-1",
+        },
+      },
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [deployerPrivateKey],
+      contracts: {
+        token: "0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846",
+      },
+      functions: {
+        router: "0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0",
+        donId: {
+          onChain: "0x66756e2d6176616c616e6368652d66756a692d31000000000000000000000000",
+          offChain: "fun-avalanche-fuji-1",
+        },
+      },
     },
   },
   // configuration for harhdat-verify plugin
