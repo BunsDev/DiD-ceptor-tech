e536690e5b424762a4dbc549278fe6d5b88bdcff
👻 <y@uklok.ai>
2024-05-24 00:05:35 -0400

    feat(hardhat): 👻 Register Request | Gateway
    - A helper function to automatically upsert the provided scripts in the gateway as requests was created.
    - It should receive only new scripts and those who needs any update.
    - minor changes in the deployment scripts to streamline the gateway contract name.
    - The gateway will be the consumer for each created subscription.

--- packages/hardhat/scripts/LINK/subscription/utils.ts
+++ packages/hardhat/scripts/LINK/subscription/utils.ts
@@ -1,13 +1,11 @@
 // Load all the scripts and verify with the mapper which are not yet deployed.
 //  return the ones that are not yet deployed or need update.
 
-import { Contract } from "ethers";
 import { SubscriptionManager } from "@chainlink/functions-toolkit";
 
 import { getNetworkConfig } from "../utils";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const consumerContractName = "CCGateway";
 let subscriptionManager: SubscriptionManager;
 
 export async function getManager(hre: HardhatRuntimeEnvironment) {
@@ -27,9 +25,4 @@ export async function getManager(hre: HardhatRuntimeEnvironment) {
   await subscriptionManager.initialize();
 
   return subscriptionManager;
-}
-
-export async function getConsumer(hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.ethers.getNamedSigners();
-  return hre.ethers.getContract<Contract>(consumerContractName, deployer);
 }
