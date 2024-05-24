d58679194d7c5e43ca486b2ddf8b0c35a4e57122
👻 <y@uklok.ai>
2024-05-24 00:24:08 -0400

    fix(hardhat): 👻 Chainlink Functions Manager
    - `@chainlink/functions-toolkit` is still using ethers@v5 which have several incompatibilities with the current project ethers@v6.
    - To resolve this problem, a workaround to generate a `Signer` using ethers@v5 using the current scope was created.
    - internal `@chainlink/functions-toolkit` ethers lib was mapped as `ethers@v5` to be accessible whenever it's needed.
    - Some npm scripts were added to easily access the new hardhat chainlink defined tasks.

--- packages/hardhat/scripts/LINK/subscription/utils.ts
+++ packages/hardhat/scripts/LINK/subscription/utils.ts
@@ -1,6 +1,7 @@
 // Load all the scripts and verify with the mapper which are not yet deployed.
 //  return the ones that are not yet deployed or need update.
 
+import { Wallet, providers } from "ethers@v5";
 import { SubscriptionManager } from "@chainlink/functions-toolkit";
 
 import { getNetworkConfig } from "../utils";
@@ -12,10 +13,17 @@ export async function getManager(hre: HardhatRuntimeEnvironment) {
   if (subscriptionManager) return subscriptionManager;
 
   const {
+    url,
     contracts: { token: linkTokenAddress },
     functions: { router: functionsRouterAddress },
   } = await getNetworkConfig(hre);
-  const signer = await hre.ethers.getNamedSigner("deployer");
+
+  // Workaround for the incompatibility between ethers v6 and v5. @chainlink/functions-toolkit is using ethers v5.
+  const deployerPrivateKey =
+    process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  const provider = new providers.JsonRpcProvider(url);
+  const signer = new Wallet(deployerPrivateKey, provider);
+  // End of workaround. goal: ethers v5 Signer with the deployer private key using the hre network.
 
   subscriptionManager = new SubscriptionManager({
     signer,
