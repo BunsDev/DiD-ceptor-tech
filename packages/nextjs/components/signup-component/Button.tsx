b119bf6fcfd6ee78f7ca45a8d2a0386f90ed2003
aldoprogrammer <aldobesma@gmail.com>
2024-05-28 22:00:52 +0700

    ref: add transition hover

--- packages/nextjs/components/signup-component/Button.tsx
+++ packages/nextjs/components/signup-component/Button.tsx
@@ -4,18 +4,23 @@ interface ButtonProps {
   text: string;
   className?: string;
   variant?: "fill" | "outline";
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, className = "", variant = "fill" }) => {
+const Button: React.FC<ButtonProps> = ({ text, onClick, className = "", variant = "fill" }) => {
   const baseStyles =
-    "text-center h-[62px] flex justify-center items-center border-2 border-solid rounded-[36.5px] duration-300 ease-in-out md:text-2xl text-xl font-oswald";
+    "text-center h-[62px] flex justify-center items-center border-2 border-solid rounded-[36.5px] duration-300 ease-in-out md:text-2xl md:hover:text-[22px] text-xl hover:text-base font-oswald";
 
   const fillStyles = "bg-amber-400 hover:bg-amber-300 text-black border-black";
   const outlineStyles = "bg-transparent text-white border-amber-400 hover:border-amber-300";
 
   const combinedStyles = `${baseStyles} ${variant === "fill" ? fillStyles : outlineStyles} ${className}`;
 
-  return <button className={combinedStyles}>{text}</button>;
+  return (
+    <button onClick={onClick} className={combinedStyles}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
