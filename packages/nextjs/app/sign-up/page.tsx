4a53cafa90723be3ea11ae5258193634c3865825
aldoprogrammer <aldobesma@gmail.com>
2024-05-28 21:59:35 +0700

    ref: tab logic for sign up

--- packages/nextjs/app/sign-up/page.tsx
+++ packages/nextjs/app/sign-up/page.tsx
@@ -1,39 +1,19 @@
 "use client";
 
 import * as React from "react";
-import Typography from "~~/components/Typography";
-import BackButton from "~~/components/signup-component/BackButton";
-import Button from "~~/components/signup-component/Button";
+import IDSignUp from "~~/components/signup-component/pages/IDSignUp";
+import JoinWithUs from "~~/components/signup-component/pages/JoinWithUs";
 
 const SignUp: React.FC = () => {
-  const handleBack = () => {
-    console.log("clik");
-  };
+  const [tab, setTab] = React.useState(0);
+
   return (
     <main
       className="flex flex-col items-center pt-5 pr-20 pb-10 pl-5
-     bg-black max-md:pr-5 h-screen"
+      bg-black max-md:pr-5 h-screen"
     >
-      <BackButton onClick={handleBack} />
-      <div
-        className="justify-center flex flex-col h-screen
-      items-center gap-10"
-      >
-        <Typography text="Commission Artist Name" variant="title" />
-        <Typography
-          text="Use the following questions to describe your character and the art that you would like Artist Name to create in their style. If you already have a character created with Ceptor Club, select the Import button and we will fill out the questions for you!"
-          variant="desc"
-        />
-        <section
-          className="grid grid-cols-1 
-      md:grid-cols-2 gap-4 w-full
-      text-center md:w-[824px] 
-      "
-        >
-          <Button text="Tell Us About Your Character" variant="fill" />
-          <Button text="Import Character from Ceptor Club" variant="outline" />
-        </section>
-      </div>
+      {tab === 0 && <JoinWithUs setTab={setTab} />}
+      {tab === 1 && <IDSignUp setTab={setTab} />}
     </main>
   );
 };
